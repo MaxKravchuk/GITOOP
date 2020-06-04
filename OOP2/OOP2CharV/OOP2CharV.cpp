3054a12cc09fc8bc3bad0e4013082bcaa701e184
MaxKravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2020-06-04 19:58:21 +0300

    LabWork 2
    LabWork 2

---
+++ OOP2/OOP2CharV/OOP2CharV.cpp
@@ -0,0 +1,145 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+
+class MyString
+{
+private:
+    char* _text = new char[500];
+    //char _text[500];
+public:
+    void Init();
+    string get();
+};
+
+class ChangeString
+{
+private:
+    MyString Text;
+public:
+    string add(string, string);
+    string Sdelete(string, int, int);
+    string SClear(string);
+    string ToUp(string, string);
+    string KeyS(string, string);
+    int AoS(string, int);
+};
+
+int main()
+{
+    system("chcp 1251");
+    system("cls");
+    MyString New;
+    MyString New2;
+    ChangeString Add;
+    ChangeString SDel;
+    ChangeString SCl;
+    ChangeString STU;
+    ChangeString SK;
+    ChangeString AS;
+    New.Init();
+    New2.Init();
+    cout << "Без змін: " << New.get() << " | " << New2.get() << endl;
+    int b1, b2;
+    cout << "Введіть позиції з якої до якої треба видалити рядок\n";
+    cout << "З: "; cin >> b1; cout << "До: "; cin >> b2;
+    string str1 = New.get();
+    string str2 = New2.get();
+    string strMain = Add.add(str1, str2);
+    string strMain1 = strMain;
+    string strMain2 = strMain;
+    string strMain3 = strMain;
+    string strMain4 = strMain;
+    string strMain5 = strMain;
+    string str4 = SDel.Sdelete(strMain1, b1, b2);
+    string str5 = SCl.SClear(strMain2);
+    string str6 = STU.ToUp(strMain, strMain3);
+    string str7 = SK.KeyS(strMain, strMain4);
+    cout << "Результат додавання: " << strMain << endl;
+    cout << "Результат видалення: " << str4 << endl;
+    cout << "Результат очищення: << " << str5 << " >>" << endl;
+    cout << "Результат приведення перших літер усіх слів тексту до верхнього регістру: \n" << str6 << endl;
+    cout << "Отримання рядка ключа: " << str7 << endl;
+    int amount;
+    cout << "Введіть кількість букв: ";
+    cin >> amount;
+    int counter = AS.AoS(strMain5, amount);
+    cout << "Кількість рядків заданої довжини: " << counter << endl;
+}
+
+void MyString::Init()
+{
+    int i = 0;
+    while (_text[i] != '\n')
+    {
+        cin >> _text[i];
+        i++;
+    }
+}
+
+string MyString::get()
+{
+    return _text;
+}
+
+string ChangeString::add(string a, string b)
+{
+    return a + b;
+}
+
+string ChangeString::Sdelete(string a, int b1, int b2)
+{
+    a.erase(b1, b2);
+    return a;
+}
+
+string ChangeString::SClear(string a)
+{
+    a.clear();
+    return a;
+}
+
+string ChangeString::ToUp(string a, string b)
+{
+    b[0] = toupper(a[0]);
+    for (int i = 1; i < size(a); i++)
+    {
+        if (isalpha(a[i]) && a[i - 1] == ' ')
+        {
+            b[i] = toupper(a[i]);
+        }
+    }
+    return b;
+}
+
+string ChangeString::KeyS(string a, string b)
+{
+    for (int i = 0; i < size(a); i++)
+    {
+        b[i] = NULL;
+    }
+
+    b[0] = a[0];
+    int c = 1;
+    for (int i = 1; i < size(a); i++)
+    {
+        if (isalpha(a[i]) && a[i - 1] == ' ')
+        {
+            b[c] = a[i];
+            c++;
+        }
+    }
+    return b;
+}
+
+int ChangeString::AoS(string a, int b)
+{
+    size_t n = b;
+    istringstream iss(a);
+    istream_iterator<std::string> beg(iss), eof;
+    size_t count = std::count_if(beg, eof, [n](std::string word) {return (word.size() <= n); });
+    return count;
+}
---
+++ OOP2/OOP2S/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace OOP2S
+{
+    class MyString //клас-рядок
+    {
+        private string _text;
+        public void Init()
+        {
+            _text = Console.ReadLine();
+        }
+        public string get()
+        {
+            return _text;
+        }
+    }
+    class ChangeString //клас методів обробки рядка
+    {
+        private MyString Text;
+        public string add(string a,string b) //ф-я додавання рядків
+        {
+            return a + b;
+        }
+        public string Sdelete(string a, int b1, int b2) //ф-я видалення певного діапазону в рядку
+        {
+            a.Remove(b1, b2);
+            return a;
+        }
+        public string SClean(string a) //ф-я очищення рядка
+        {
+            return a.Remove(0);
+        }
+       public string ToUp(string a) //ф-я приведення перших букв до верхнього регістру
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            a = textInfo.ToTitleCase(a);
+            return a;
+        }
+        public int AoS(string a,int b) //ф-я підрахунку к-сті слів певної довжини
+        {
+            int cnt = 0;
+            string[] words = a.Split('\n',' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length < b+1) cnt++;
+            }
+            return cnt;
+        }
+        public string KeyS(string a, string b) //ф-я створення рядка ключа
+        {
+            b = String.Empty;
+            string[] array = a.Split(' ');
+            for (int i = 0; i < array.Length; i++)
+            {
+                b += array[i][0];
+            }
+            return b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode; //підключення укр.мови
+            MyString New = new MyString(); //створення об'єкту
+            MyString New2 = new MyString();
+            ChangeString Add = new ChangeString(); //створення об'єкту
+            ChangeString SDel = new ChangeString();
+            ChangeString SCl = new ChangeString();
+            ChangeString STU = new ChangeString();
+            ChangeString SK = new ChangeString();
+            ChangeString AS = new ChangeString();
+            New.Init(); //ініціалізація об'єкту
+            New2.Init();
+            Console.WriteLine("Без змін: {0} | {1}", New.get(), New2.get());
+            int b1, b2; //границі видалення
+            Console.WriteLine("Введіть позиції з якої до якої треба видалити рядок");
+            Console.Write("З: "); b1 = Convert.ToInt32(Console.ReadLine()); //введення змінних
+            Console.Write("До: "); b2 = Convert.ToInt32(Console.ReadLine());
+            string str1 = New.get(); //створення рядків для їх обробки
+            string str2 = New2.get();
+            string strMain = Add.add(str1, str2);
+            string strMain1 = strMain;
+            string strMain2 = strMain;
+            string strMain3 = strMain;
+            string strMain4 = strMain;
+            string strMain5 = strMain;
+            string str4 = SDel.Sdelete(strMain1, b1, b2);
+            string str5 = SCl.SClean(strMain2);
+            string str6 = STU.ToUp(strMain3);
+            string str7 = SK.KeyS(strMain, strMain4);
+            Console.WriteLine("Результат додавання: {0}",strMain);
+            Console.WriteLine("Результат видалення: {0}", str4);
+            Console.WriteLine("Результат очищення: << {0} >>", str5);
+            Console.WriteLine("Результат приведення перших літер усіх слів тексту до верхнього регістру: {0}", str6);
+            Console.WriteLine("Отримання рядка ключа: {0}", str7);
+            int amount;
+            Console.Write("Введіть кількість букв: ");
+            amount = Convert.ToInt32(Console.ReadLine());
+            int counter = AS.AoS(strMain5, amount);
+            Console.WriteLine("Кількість рядків заданої довжини: {0}", counter);
+        }
+    }
+}
